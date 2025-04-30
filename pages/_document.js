91a5d457c0f1ab487248e4e87621fed0ebb5900b
Medi G Fulgence <lycorisblue99@gmail.com>
2025-04-30 05:57:54 +0000

    Mise à jour de la langue du document HTML en français

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="fr">
       <Head>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
